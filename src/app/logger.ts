916719d8abe4995846238b6c25e3f44ef4269792
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2022-04-10 23:46:40 -0400

    fix: Logger only send log to default stdout

--- src/app/logger.ts
+++ src/app/logger.ts
@@ -1,17 +1,11 @@
 import { createLogger, format, transports } from 'winston';
 
 export const logger = createLogger({
-  level: 'info',
   format: format.combine(
     format.errors({ stack: true }),
     format.timestamp(),
     format.json(),
   ),
   defaultMeta: { service: process.env.APPLICATION_NAME },
-  transports: [
-    new transports.File({ filename: 'application.log' }),
-    new transports.Console({
-      format: format.combine(format.colorize()),
-    }),
-  ],
+  transports: [new transports.Console()],
 });
